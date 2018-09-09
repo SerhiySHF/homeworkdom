154826664b8b7e1c3a8c70440148efa5e2e61062
Serhiy Mykytiuk <serhiyshf@bigmir.net>
2018-09-09 11:42:46 +0000

    calc

---
+++ js/calculator.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.calc .digits-oper')
+    .forEach( el => el.addEventListener('click', pressedDigitOrOper));
+    
+    
+function pressedDigitOrOper(ev) {
+    const displ = document.querySelector('.calc .displ');
+    displ.value += ev.target.innerText;
+}
+
+document.querySelector('.calc .eval')
+    .addEventListener('click', evaluate);
+
+function evaluate() {
+    const displ = document.querySelector('.calc .displ');
+    displ.value = eval(displ.value);
+}
+  
